7f03ae4d0ebe6529a3b5f4faaedb0ff23d7a6e54
AndresPataFM <apfm21@Gmail.com>
2025-12-17 20:53:10 -0300

    inicio clase 2

---
+++ clase-2/clase2-vivo/clase-2-vivo.js
@@ -0,0 +1,12 @@
+/* "7"
+'7'
+typeof "7"
+'string'
+parseInt("7")
+7
+typeof parseInt("7")
+'number'
+"7" -5
+2
+7 - 5
+2 */
---
+++ clase-4/ejemplo-primera-pre-entrega-array/mini/pseudoProyecto.js
@@ -0,0 +1,38 @@
+//Este pseudo proyecto es para una version previa de flex en la cual la primera entrega incluia solo calses 1 a 3
+
+let ingreso = false
+let intentos = 0
+let nombre = prompt("Ingrese su nombre")
+let nota = 0
+
+function login(){
+  let contrasenia = prompt("Ingrese la contrasenia")
+  return contrasenia === "123"
+}
+
+function examen(){
+  nota = 0
+  for (let i =0 ; i<5; i++){
+    let pregunta = confirm("correcto? ")
+    if(pregunta){
+      nota += 2
+    }
+  }
+  alert(nota)
+  return nota
+}
+
+while(!ingreso){
+  ingreso = login()
+  intentos++
+  if(intentos === 3){
+    alert("te quedaste sin intentos")
+    break
+  }
+}
+if(ingreso){
+  // ejecuto la página
+  alert(`Bienvenido ${nombre}`)
+  examen()
+}
+
