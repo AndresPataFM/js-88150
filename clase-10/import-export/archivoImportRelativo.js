c92452dc5de735dd603267de807f846b4d5e889a
AndresPataFM <apfm21@Gmail.com>
2026-02-25 20:43:58 -0300

    inicio clase 10

---
+++ clase-10/import-export/archivoImportRelativo.js
@@ -0,0 +1,13 @@
+import { saludar, despedir, nombre } from "./archivoExport.js";
+import sumar from "./exportDefault.js";
+import helper from "./archivoExport copy.js"
+// relativo entre los archivos
+
+saludar()
+despedir()
+
+helper.saludar()
+
+console.log(nombre)
+
+console.log(sumar(3,8))
---
+++ clase-10/testingNode.js
@@ -0,0 +1,26 @@
+// console.log("hola")
+// console.table([1,2,3,4,5])
+// console.log({})
+
+// No hay document ni herramientas relacionadas (window no existe)
+// alert("soy un alert")
+// document.createElement("div")
+// localStorage
+
+
+// const fs = require("node:fs")
+
+// crear
+// fs.writeFileSync("ejemplo.txt", "Hola, me crearon con node.")
+
+// leer
+/* fs.readFile("ejemplo.txt", "utf8", (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+}); */
+
+// agregar al final | apend
+// fs.appendFileSync("ejemplo.txt", " A mi me agregaron con node.")
